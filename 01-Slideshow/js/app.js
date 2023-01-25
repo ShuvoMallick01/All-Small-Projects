e1f4370f75ccb6329c2059a8c0eafb3f189c296c
ShuvoMallick01 <shuvacse01@gmail.com>
2023-01-25 21:45:53 +0600

    Fixing Issues

--- 01-Slideshow/js/app.js
+++ 01-Slideshow/js/app.js
@@ -15,6 +15,27 @@ function showSlides(n) {
   let i;
   let slides = document.getElementsByClassName("mySlides");
   let dots = document.getElementsByClassName("dot");
+  console.log(n);
+  console.log(slides.length);
+  console.log(slideIndex);
+  //   console.log(slides.length, n);
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
 
-  if(n > slides.length)
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+    // console.log(dots[i].className);
+  }
+
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
 }
