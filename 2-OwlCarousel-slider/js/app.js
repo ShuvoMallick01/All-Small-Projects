fb5e26ccfd7d3edd18aee9bb8b4ddc5defedc49e
ShuvoMallick01 <shuvacse01@gmail.com>
2023-02-12 22:20:28 +0600

    New Commit for Relink

---
+++ 2-OwlCarousel-slider/js/app.js
@@ -0,0 +1,11 @@
+// console.log(tns);
+console.log(SVGInject);
+
+const slider = tns({
+  container: ".slider",
+  items: 2,
+  gutter: 10,
+  // controls: false,
+  controlsPosition: "bottom",
+  autoplay: true,
+});
